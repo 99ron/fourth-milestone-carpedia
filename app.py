b1fd607c9db032ac8e2d576f5d7c078ff8b422c8
99ron <azza_5252@hotmail.com>
2019-04-04 15:25:35 +0000

    Added Login and Register function to local database. Action tweaks done to login and register pages.

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 # Test app.py 
 import os
-import pymysql
 import sqlalchemy
-from flask import Flask, render_template, redirect, request, url_for
+from model import *
+from flask import Flask, render_template, redirect, request, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
+
+
+DB_URI = 'mysqldb://bd6203f445759d:3f8eca2f@eu-cdbr-west-02.cleardb.net/heroku_1146a0b312400c5?reconnect=true'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysqldb://bd6203f445759d:3f8eca2f@eu-cdbr-west-02.cleardb.net/heroku_1146a0b312400c5?reconnect=true'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 
 @app.route('/')
@@ -19,6 +22,36 @@ def login():
 
 @app.route('/register')
 def index():
+    return render_template('register.html')
+
+@app.route('/welcome', methods=['GET', 'POST'])
+def welcome():
+    return render_template('base.html')
+
+
+@app.route('/check-user-credentials', methods=['GET','POST'])
+def checkuser():
+    if request.method == "POST":
+        uname = request.form['username']
+        pword = request.form['password']
+        login = Users.query.filter_by(username=uname, password=pword).first()
+        
+        if login is not None:
+            return redirect (url_for('welcome'))
+           # flash('wrong password')
+        return render_template('login.html')
+
+@app.route('/register-user/', methods=['GET', 'POST'])
+def register():
+	
+    if request.method == 'POST':
+	    uname = request.form['username']
+	    pword = request.form['password']
+	    new_user = Users(username = uname, password = pword)
+    db.session.add(new_user)
+    db.session.commit()
+		
+    return redirect(url_for('login'))
     return render_template('register.html')
     
 
--- model.py
+++ model.py
@@ -1,11 +1,22 @@
 from app import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import *
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine, Table, Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+engine = create_engine('sqlite:///site.db', echo=True)
+
+
 
 class Users(db.Model):
     __tablename__ = 'Users'
     user_id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(15), unique = True)
-    password = db.Column(db.String(15), unique = True)
+    password = db.Column(db.String(15))
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
