bdc1f7ac9684677800c36f497fc8a32df218a74d
99ron <azza_5252@hotmail.com>
2019-07-04 11:05:29 +0000

    Added commens in script.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     $('.collapsible').collapsible();
     $('select').material_select();
     $(".button-collapse").sideNav();
+    
+    // Function for scroll to top. Fades in when greater than 100px from the top.
     $(window).scroll(function(){ 
         if ($(this).scrollTop() > 100) { 
             $('#scroll').fadeIn(); 
@@ -10,8 +12,12 @@ $(document).ready(function() {
             $('#scroll').fadeOut(); 
         } 
     }); 
+    
+    // Once button is clicked it'll scroll to the top.
     $('#scroll').click(function(){ 
         $("html, body").animate({ scrollTop: 0 }, 600); 
         return false; 
-    }); 
+    });
+    
 });
+
